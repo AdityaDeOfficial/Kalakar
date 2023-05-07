dc81ea6874e5cf95604b74e5077a8b8ecd91d7c9
Aditya De <adityadeofficial@gmail.com>
2023-05-07 22:32:01 +1000

    basic manga crud is alright except d

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "mangas#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :mangas do
+    resources :chapters
+  end
 end
