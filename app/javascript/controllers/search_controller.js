b57732974494326e79f7cc1055f8b7684f7e661c
Philip Curry <muttaphilly@gmail.com>
2023-05-13 14:59:18 +0800

    still broke

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,11 +4,33 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "query", "results" ]
 
-  submit(event) {
+  connect() {
+    this.resultsTarget.classList.add("hidden")
+  }
+
+  async search_manga() {
+    const query = this.queryTarget.value
+    fetch(`/mangas?search=${query}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    } )
+    .then(response => response.json())
+    .then(data => console.log(data))
+    console.log(query)
+  }
+
+  submit_manga(event) {
     event.preventDefault()
+    console.log("working")
     const query = this.queryTarget.value
     fetch(`/mangas?search=${query}`)
       .then(response => response.text())
       .then(html => this.resultsTarget.innerHTML = html)
   }
+
+  //TODO popup below search bar
+  // populates suggestions as type
+  //remove search button
 }
