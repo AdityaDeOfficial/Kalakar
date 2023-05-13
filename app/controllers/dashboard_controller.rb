25f3afdfb09bab66130d080cedc8971a9e69ee58
Mary Chan <marychancfy@gmail.com>
2023-05-14 01:37:19 +1000

    front end dashboard and landing page basic done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    # #display bookmark belongs to user show 5 ? to see more go to my bookmark.all page
+    # @bookmarks = current_user.bookmarks #works as an array
+
+    #display manga belongs to user show 10? to see more go to my manga.all page
     @mangas = current_user.mangas
+
     @favorited_mangas = current_user.all_favorited
   end
 end
