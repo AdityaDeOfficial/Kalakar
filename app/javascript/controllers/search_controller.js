64e54ccb5ac63d5a38e4d5390b261c21a05ff700
Philip Curry <muttaphilly@gmail.com>
2023-05-23 19:38:48 +0800

    search pathing working

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -7,11 +7,7 @@ export default class extends Controller {
   connect() {
    console.log("yooooooooo")
   }
-  // submitForm(event) {
-  //   event.preventDefault();
-  //   console.log('Submitting form');
-  //   this.formTarget.submit();
-  // }
+
   displayResults(data) {
     const form = this.dropdownTarget;
     const fullForm = this.formTarget;
@@ -23,23 +19,20 @@ export default class extends Controller {
       form.appendChild(optionElement);
     });
     form.size = form.length;
-    const multiplier = form.length * 23;
+    const multiplier = form.length * 40;
     const dd = document.getElementById("dropdown-select");
     dd.style.height = multiplier + "px";
     console.log(dd);
     Array.from(dd).forEach(option => {
-      console.log("Value: " + option.value + ", Text: " + option.text);
       option.addEventListener("click", (e) => {
-        console.log(e.target.innerHTML)
-        // fullForm.querySelector("input[name='myInput']").value = e.target.innerHTML;
-        console.log(fullForm)
-        fullForm.submit();
+        const selectedOption = e.target.innerHTML;
+        const selectedManga = data.find(option => option.manga_title === selectedOption);
+        if (selectedManga) {
+          window.location.href = `/mangas/${selectedManga.id}`;
+        }
       })
     });
-    // dd.forEach(value => {
-    //   console.log(value)
-    // })
-    // this.formTarget.submit();
+
   };
   async searchManga() {
     const query = this.queryTarget.value;
@@ -48,7 +41,6 @@ export default class extends Controller {
       const response = await fetch(`/mangas?search=${query}`);
       const data = await response.json();
       this.displayResults(data);
-      // this.openForm();
     } catch (error) {
       console.error(error);
     }
